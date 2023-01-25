bfb1b218736be79b4173f34ef902f9d2377200f1
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2023-01-24 19:24:42 -0500

    Initialize the project

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require(`mongoose`);
+const Reaction = require(`./Reaction`);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                return date.toISOString().split("T")[0]
+            }
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [Reaction]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+);
